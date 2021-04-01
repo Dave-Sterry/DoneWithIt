1e573ed5609fa04e619ceee92cac56959e0da8d5
David Sterry <sterry.david@gmail.com>
2021-04-01 11:02:19 -0700

    try out console log for onPress in Text component

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
-
+  const handlePress = () => console.log("text pressed");
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello React Native! I'm going to make this a ton of text so that it doesn't look good in the simulator</Text>
 
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
   },
 });
