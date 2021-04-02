cb2ef48c9673091a38e545e8ee8f7f571e7d7790
David Sterry <sterry.david@gmail.com>
2021-04-01 20:27:40 -0700

    build out welcome screen

--- App.js
+++ App.js
@@ -1,47 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
 export default function App() {
-
-  return (
-    <View style={{
-      backgroundColor: "#fff",
-      flex: 1,
-      flexDirection: "row", //sets main axis
-      justifyContent: "center", //main axis alignment
-      alignItems: "center", //secondary axis
-      flexWrap: "wrap",
-    }}>
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        width: 100,
-        height: 300,
-
-      }} />
-      <View style={{
-        backgroundColor: 'gold',
-        width: 100,
-        height: 100,
-      }} />
-      <View style={{
-        backgroundColor: 'tomato',
-        width: 100,
-        height: 100,
-      }} />
-      <View style={{
-        backgroundColor: 'grey',
-        width: 100,
-        height: 100,
-      }} />
-      <View style={{
-        backgroundColor: 'greenyellow',
-        width: 100,
-        height: 100,
-      }} />
-
-    </View>
-
-  );
+  return <WelcomeScreen />
 }
 
