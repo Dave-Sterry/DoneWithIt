05c8c8c4566ac2f384363904e57ffe499a9815d8
David Sterry <sterry.david@gmail.com>
2021-04-01 20:42:14 -0700

    add view image screen, colors.js file

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
 export default function App() {
-  return <WelcomeScreen />
+  return <ViewImageScreen />
 }
 
