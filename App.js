7d4147c5a7843b4fcc5a5328775f92bde5075490
David Sterry <sterry.david@gmail.com>
2021-05-19 16:07:13 -0700

    add listitem screen and style

--- App.js
+++ App.js
@@ -5,21 +5,8 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppButton from "./app/components/AppButton.js";
 import AppText from "./app/components/AppText.js";
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      ></Card>
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
