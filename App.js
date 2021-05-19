17b0c28eb94ecf1834bfdbc500b1ede89b828d30
David Sterry <sterry.david@gmail.com>
2021-05-19 16:11:42 -0700

    add icons to viewimagescreen

--- App.js
+++ App.js
@@ -8,5 +8,5 @@ import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
