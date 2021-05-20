b842e457292a56f343f7bc28cafd1ddf7267211d
David Sterry <sterry.david@gmail.com>
2021-05-20 13:19:43 -0700

    fix delete swipe size

--- app/components/ListItemDeleteAction.js
+++ app/components/ListItemDeleteAction.js
@@ -18,6 +18,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.danger,
     width: 70,
+    height: "100%",
     justifyContent: "center",
     alignItems: "center",
   },
