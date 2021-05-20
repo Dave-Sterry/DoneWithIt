97849cb18625eeb2661e6e82539b74067a4ce011
David Sterry <sterry.david@gmail.com>
2021-05-20 13:31:36 -0700

    add resuable icon component

--- App.js
+++ App.js
@@ -7,7 +7,13 @@ import AppText from "./app/components/AppText.js";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={100} />
+    </Screen>
+  );
 }
