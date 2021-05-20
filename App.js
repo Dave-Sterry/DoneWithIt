2b69e0d4a9e1f0d32aa62569a11f497333195213
David Sterry <sterry.david@gmail.com>
2021-05-20 14:17:51 -0700

    add listing screen for items to sell

--- App.js
+++ App.js
@@ -11,7 +11,8 @@ import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
+import ListingScreens from "./app/screens/ListingScreens";
 
 export default function App() {
-  return <AccountScreen />;
+  return <ListingScreens />;
 }
--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Constants from "expo-constants";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={style}>{children}</View>
+    </SafeAreaView>
+  );
 }
 const styles = StyleSheet.create({
   screen: {
