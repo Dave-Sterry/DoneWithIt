96b52d23322b3c38d286eafd14d59765ca559a2e
David Sterry <sterry.david@gmail.com>
2021-05-21 12:14:32 -0700

    test out a switch, super neat

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Switch, Text, TextInput, View } from "react-native";
+
 import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 
 export default function App() {
   const [firstName, setFirstName] = useState("");
-
+  const [isNew, setIsNew] = useState(false);
   return (
     <Screen>
-      <AppTextInput placeholder="username" icon="email" />
+      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
+
+      {/* <AppTextInput placeholder="username" icon="email" /> */}
     </Screen>
   );
 }
