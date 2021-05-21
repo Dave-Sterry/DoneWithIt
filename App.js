739e8830cee873822a98cc82d740bbd5694e46b3
David Sterry <sterry.david@gmail.com>
2021-05-21 11:46:58 -0700

    mess around with TextInput

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import React from "react";
-import { View } from "react-native";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import AppButton from "./app/components/AppButton.js";
-import AppText from "./app/components/AppText.js";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import React, { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingScreens from "./app/screens/ListingScreens";
 
 export default function App() {
-  return <ListingScreens />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        keyboardType="numeric"
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="first name"
+        style={{
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      />
+    </Screen>
+  );
 }
