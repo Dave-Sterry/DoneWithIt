14aaced36bd493e770593f9d3689de8476fc9da9
David Sterry <sterry.david@gmail.com>
2021-05-25 15:39:14 -0700

    add modal with slide animation to picker

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,29 +1,50 @@
 import React from "react";
-import { TextInput, View, StyleSheet, Platform } from "react-native";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Platform,
+  TouchableWithoutFeedback,
+  Modal,
+  Button,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import defaultStyles from "../config/styles";
 import AppText from "./AppText";
 import AppTextInput from "./AppTextInput";
+import { useState } from "react/cjs/react.development";
+import Screen from "./Screen";
 
 function AppPicker({ icon, placeholder, ...otherProps }) {
+  const [modalVisable, setModalVisable] = useState(false);
+
   return (
-    <View style={styles.container}>
-      {icon && (
-        <MaterialCommunityIcons
-          name={icon}
-          size={20}
-          color={defaultStyles.colors.medium}
-          style={styles.icon}
-        />
-      )}
-      <AppText style={styles.text}>{placeholder}</AppText>
-      <MaterialCommunityIcons
-        name="chevron-down"
-        size={20}
-        color={defaultStyles.colors.medium}
-      />
-    </View>
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisable(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={defaultStyles.colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>{placeholder}</AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={defaultStyles.colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisable} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisable(false)} />
+        </Screen>
+      </Modal>
+    </>
   );
 }
 const styles = StyleSheet.create({
