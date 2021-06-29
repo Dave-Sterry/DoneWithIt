3f5b92dd79532fd33e0559820396cf0b055b2722
David Sterry <sterry.david@gmail.com>
2021-06-29 15:25:36 -0700

    update ImageInput with delete ability

--- App.js
+++ App.js
@@ -26,9 +26,10 @@ export default function App() {
 
   return (
     <Screen>
-      <Button title="Select Image" onPress={selectImage} />
-      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-      <ImageInput imageUri={imageUri} />
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
     </Screen>
   );
 }
